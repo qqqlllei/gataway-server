05c3e022cc5ff7985b01d1a06a13474019bf0947
lilei <861971291@qq.com>
2018-09-07 16:33:19 +0800

    add post loan system

--- src/main/java/com/gataway/server/fallback/AuthFeginFallBack.java
+++ src/main/java/com/gataway/server/fallback/AuthFeginFallBack.java
@@ -1,6 +1,7 @@
 package com.gataway.server.fallback;
 
 import com.alibaba.fastjson.JSONObject;
+import com.netflix.client.ClientException;
 import com.netflix.hystrix.exception.HystrixTimeoutException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.net.SocketTimeoutException;
 
 /**
  * Created by 李雷 on 2018/8/8.
@@ -26,6 +28,8 @@ public class AuthFeginFallBack implements FallbackProvider {
     public ClientHttpResponse fallbackResponse(Throwable cause) {
         if (cause instanceof HystrixTimeoutException) {
             return response(HttpStatus.GATEWAY_TIMEOUT);
+        }else if(cause instanceof ClientException){
+            return response(HttpStatus.REQUEST_TIMEOUT);
         } else {
             return fallbackResponse();
         }
@@ -68,6 +72,7 @@ public class AuthFeginFallBack implements FallbackProvider {
                 JSONObject body = new JSONObject();
                 body.put("code","200");
                 body.put("message","微服务故障, 请稍后再试");
+                body.put("error",getRawStatusCode());
                 return new ByteArrayInputStream(body.toJSONString().getBytes());
             }
 
